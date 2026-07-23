5551ea6930aa51c74ac6dd761ff6b8c20965f536
Medhawin Tiwari <MEDHAWINTIWARI12@GMAIL.COM>
2026-07-23 21:46:22 +0530

    Time: 15 ms (30.54%), Space: 35 MB (88.83%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,31 +1,33 @@
 class Solution {
 public:
-int finddays(vector<int> &weights, int cap) {
-    int days = 1, load = 0;
-    for (int i =0; i<weights.size();i++) {
-        if (weights[i] + load > cap) {
-            days +=1;
-            load = weights[i];
+    int finddays(int cap, vector<int>& wt) {
+        int day = 1, load = 0;
+        for (int i = 0; i < wt.size(); i++) {
+            if (load + wt[i] > cap) {
+                day += 1;
+                load = wt[i];
+            } else {
+                load += wt[i];
+            }
         }
-        else {
-            load += weights[i];
-        }
+        return day;
     }
-    return days;
-}
     int shipWithinDays(vector<int>& weights, int days) {
-      int low = *max_element(weights.begin(), weights.end()) ;
-       int high = accumulate (weights.begin(),weights.end(),0);
-       while (low <=high) {
-        int mid = (low+high) /2;
-        int numberofdays = finddays(weights, mid);
-        if (numberofdays <= days) {
-            high = mid -1;
+        // range of capacity will be from the max value of weights till the
+        // total sum of weights reason: the ship will only be able to carry
+        // everything if its capacity is atleast equal or more than equal to the
+        // largest weight the sum of weights will get the work done in 1 day
+        int low = *max_element(weights.begin(), weights.end());
+        int high = accumulate(weights.begin(), weights.end(), 0);
+        while (high >= low) {
+            int mid = (low + high) / 2;
+            int numberofdays = finddays(mid, weights);
+            if (numberofdays <= days) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
         }
-        else {
-            low = mid + 1;
-        }
-       }
-       return low;
+        return low;
     }
 };
