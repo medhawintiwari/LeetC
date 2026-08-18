0d2db0fe37f377afb147c6567a3967f962655d11
Medhawin Tiwari <MEDHAWINTIWARI12@GMAIL.COM>
2026-08-18 22:03:43 +0530

    Time: 3 ms (43.96%), Space: 8.7 MB (36.33%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) return false; // negative numbers are never palindrome
+
+        long long rev = 0;
+        int temp = x;
+
+        while (temp != 0) {
+            rev = rev * 10 + temp % 10;
+            temp /= 10;
+        }
+
+        return rev == x;
+    }
+};
