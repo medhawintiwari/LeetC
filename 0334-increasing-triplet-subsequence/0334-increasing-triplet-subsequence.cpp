746e4ba1c4f957b5ff35a5d134c50ff31b4aabcf
Medhawin Tiwari <MEDHAWINTIWARI12@GMAIL.COM>
2026-08-21 15:35:19 +0530

    Time: 0 ms (100%), Space: 115.6 MB (92%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
@@ -3,14 +3,14 @@ public:
     bool increasingTriplet(vector<int>& nums) {
         int first = INT_MAX;
         int second = INT_MAX;
-
         for (int num : nums) {
-            if (num <= first) {
-                first = num; // Update smallest element found so far
-            } else if (num <= second) {
-                second = num; // Update second smallest element
-            } else {
-                // If num is greater than both first and second, we found a triplet!
+            if (num<=first) {
+                first = num;
+            }
+            else if (num<=second){
+                second = num;
+            }
+            else {
                 return true;
             }
         }
