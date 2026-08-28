f539fccca9e0f2595be6ea2fe314002fc7bb3c1c
Medhawin Tiwari <MEDHAWINTIWARI12@GMAIL.COM>
2026-08-28 21:34:27 +0530

    Time: 0 ms (100%), Space: 7.7 MB (65.55%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     int fib(int n) {
         if ( n <=1 ) return n;
-        int prev2= 0;
-        int prev1=1;
+        int prev2= 0; //F(0)
+        int prev1=1;  //F(1)
         for (int i=2; i<=n; i++) {
             int current = prev1+ prev2;
             prev2=prev1;
