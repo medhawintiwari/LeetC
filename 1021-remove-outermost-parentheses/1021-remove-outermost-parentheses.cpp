f1f85c7add25faadb0069276627bcba0e77e5aac
Medhawin Tiwari <MEDHAWINTIWARI12@GMAIL.COM>
2026-09-13 13:45:27 +0530

    Time: 0 ms (100%), Space: 8.9 MB (54.03%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        string result = "";
+        int balance = 0;
+        for (char c : s) {
+            if (c=='(') {
+                if(balance>0) {
+                    result+=c;
+                }
+                balance++;
+            }
+            else {  //c==")"
+                balance--;
+                if(balance>0){
+                    result+=c;
+                }
+            }
+        }
+        return result;
+    }
+};
